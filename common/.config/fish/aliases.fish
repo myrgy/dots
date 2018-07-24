74ef8135e3f28e1acc6426e24c57b8d836ff35ea
Alexander Dalshov <adalshov@charlesworthresearch.com>
2018-07-24 15:37:48 +0300

    fish: add more aliases

---
+++ common/.config/fish/aliases.fish
@@ -0,0 +1,11 @@
+alias cd.. 'cd ..'
+alias .. 'cd ..'
+alias ... 'cd ../..'
+alias .... 'cd ../../..'
+alias ..... 'cd ../../../..'
+
+alias l ls
+alias ll 'ls -la'
+
+# Colorized cat (will guess file type based on contents)
+alias ccat 'pygmentize -g'
--- common/.config/fish/config.fish
+++ common/.config/fish/config.fish
@@ -1,2 +1,3 @@
+. ~/.config/fish/aliases.fish
 set -gx PATH ~/.opt/bin $PATH
 # neofetch --disable packages --ascii_distro deepin
